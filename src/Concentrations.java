8b44d02d2109c017f1443d48c358f290ee3a3de9
noman17rus <noman171991@gmail.com>
2020-01-31 13:02:31 +0400

    init3

--- src/Concentrations.java
+++ src/Concentrations.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Concentrations {
+
+    public static float hardness(float volimeHARD1, float volimeHARD2, double trillonB, int volume) {
+        float hardness = (float)(((2*volimeHARD1*trillonB*1000)/volume)+((2*volimeHARD2*trillonB*1000)/volume))/2;
+        return hardness;
+    };
+    public static float calcium (float volumecalcium1, float volumecalcium2, double trillonB, int volume){
+        float calcium = (float)(((40.08*trillonB*volumecalcium1*1000)/volume)+((40.08*trillonB*volumecalcium2*1000)/volume))/2;
+        return calcium;
+    };
+    public static float magnesium (float volimeHARD1, float volumecalcium1, float volumecalcium2, float volimeHARD2,double trillonB, int volume){
+        float magnesium = (float)((((volimeHARD1-volumecalcium1)*trillonB*24.32*1000)/volume)+(((volimeHARD2-volumecalcium2)*trillonB*24.32*1000)/volume))/2;
+        return magnesium;
+    };
 }
--- src/Hardness.java
+++ src/Hardness.java
@@ -1,17 +1,21 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Hardness {
 
     public static void main (String args[]){
 
         int volume=50; //объём пробы
         double concentration=0.00971; // концентрация трилона Б
-        float volimeHARD1 = (float) 7.9;
-        float volimeHARD2 = (float) 8.0;
-        float volumecalcium1 = (float) 3.5;
-        float volumecalcium2 = (float) 3.6;
-        float hardRES = (float)(((2*volimeHARD1*concentration*1000)/volume)+((2*volimeHARD2*concentration*1000)/volume))/2;
-        float calciumRES =(float)(((40.08*concentration*volumecalcium1*1000)/volume)+((40.08*concentration*volumecalcium2*1000)/volume))/2;
-        float magnesiumRES = (float)((((volimeHARD1-volumecalcium1)*concentration*24.32*1000)/volume)+(((volimeHARD2-volumecalcium2)*concentration*24.32*1000)/volume))/2;
-        float normHARD = (float) (hardRES * 0.06);
+        float volimeHARD1 = (float) 12.9;
+        float volimeHARD2 = (float) 13.0;
+        float volumecalcium1 = (float) 6.5;
+        float volumecalcium2 = (float) 6.7;
+        float hardRES = Concentrations.hardness(volimeHARD1,volimeHARD2,concentration,volume);
+        float calciumRES = Concentrations.calcium(volumecalcium1,volumecalcium2,concentration,volume);
+        float magnesiumRES = Concentrations.magnesium(volimeHARD1,volumecalcium1,volumecalcium2,volimeHARD2,concentration,volume);
+        float normHARD = (float) (hardRES * 0.06); //норматив для жесткости
+        //норматив для кальция
         float normCalc;
         if (calciumRES <= 2) {
             normCalc = (float) (calciumRES*0.22);
@@ -27,6 +31,7 @@ public class Hardness {
         float deltaCalc = Math.abs(volumecalcium1 - volumecalcium2);
         float deltaMagnesium = (float)Math.abs((((volimeHARD1-volumecalcium1)*concentration*24.32*1000)/volume)-(((volimeHARD2-volumecalcium2)*concentration*24.32*1000)/volume));
 
+        //проверка на условие сходимости
         boolean truecalc;
         boolean trueHard;
         boolean truMagnesium;
@@ -42,17 +47,53 @@ public class Hardness {
             truMagnesium = true;
         } else truMagnesium = false;
 
-        float pogreshnost;
+        //погрешности
+        float pogrCalc;
+        float pogrHard = (float) (hardRES*0.09);
+        float pogrMagn = (float) (magnesiumRES*0.02);
         if (calciumRES <= 2) {
-            pogreshnost = (float)(calciumRES*0.25);
+            pogrCalc = (float)(calciumRES*0.25);
         } else if (calciumRES <= 10) {
-            pogreshnost = (float)(calciumRES*0.15);
-        } else pogreshnost = (float)(calciumRES *0.11);
+            pogrCalc = (float)(calciumRES*0.15);
+        } else pogrCalc = (float)(calciumRES *0.11);
 
         if (trueHard == true && truecalc == true && truMagnesium == true) {
-            System.out.println("Жесткость " +hardRES +" погрешность " +(float)(hardRES*0.09));
-            System.out.println("Кальций " +calciumRES +" погрешность " +pogreshnost);
-            System.out.println("Магний " +magnesiumRES +" погрешность " +(float)(magnesiumRES*0.02));
+            System.out.print("Жесткость ");
+            Hardness.znakiposlezapyatoy(hardRES,pogrHard);
+            System.out.print("Кальций ");
+            Hardness.znakiposlezapyatoy(calciumRES,pogrCalc);
+            System.out.print("Магний ");
+            Hardness.znakiposlezapyatoy(magnesiumRES,pogrMagn);
         } else System.out.println("не удовлетворяет условию сходимости");
+    }
+
+     public static void znakiposlezapyatoy (float result, float pogreshnost) {
+         BigDecimal resPOGR = new BigDecimal(pogreshnost);
+         BigDecimal res = new BigDecimal(result);
+         if (pogreshnost >= 3) {
+             resPOGR = resPOGR.setScale(0, RoundingMode.HALF_EVEN);
+             res = res.setScale(0, RoundingMode.HALF_EVEN);
+             System.out.println(res + " ± " + resPOGR);
+         }
+         if (pogreshnost <= 3 & pogreshnost >= 0.3) {
+             resPOGR = resPOGR.setScale(1, RoundingMode.HALF_EVEN);
+             res = res.setScale(1, RoundingMode.HALF_EVEN);
+             System.out.println(res + " ± " + resPOGR);
+         }
+         if (pogreshnost <= 0.3 & pogreshnost >= 0.03) {
+             resPOGR = resPOGR.setScale(2, RoundingMode.HALF_EVEN);
+             res = res.setScale(2, RoundingMode.HALF_EVEN);
+             System.out.println(res + " ± " + resPOGR);
+         }
+         if (pogreshnost <= 0.03 & pogreshnost >= 0.003) {
+             resPOGR = resPOGR.setScale(3, RoundingMode.HALF_EVEN);
+             res = res.setScale(3, RoundingMode.HALF_EVEN);
+             System.out.println(res + " ± " + resPOGR);
+         }
+         if (pogreshnost < 0.003 & pogreshnost >= 0.0003) {
+             resPOGR = resPOGR.setScale(4, RoundingMode.HALF_EVEN);
+             res = res.setScale(4, RoundingMode.HALF_EVEN);
+             System.out.println(res + " ± " + resPOGR);
+         };
     }
 }
